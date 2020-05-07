b768de257994dc166cff484ad38a3d842c85e9cf
Siddhant Mulay <siddhant@herringboneandsui.com>
2020-05-07 16:30:11 +0530

    Tweaks

--- src/common/config.js
+++ src/common/config.js
@@ -1,10 +1,10 @@
 
 const config = {
-    API_URL: '	https://api.cloudinary.com/v1_1/yobucket',
+    API_URL: '	https://api.cloudinary.com/v1_1/sid-img-ops-bucket',
     API_BASE_URL: '	https://api.cloudinary.com/',
     API_KEY: '911828417765431',
     API_SEC: 'wAlstcmrCu1awCdBexA8b5ZaNxQ',
-    CLOUD_NAME: 'yobucket'
+    CLOUD_NAME: 'sid-img-ops-bucket'
 }
 
 
