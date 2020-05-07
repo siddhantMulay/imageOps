ecea3ce32dd901853ae9dad5e85fbca77850c8e1
Siddhant Mulay <siddhant@herringboneandsui.com>
2020-05-07 16:15:03 +0530

    Image config almost done...

--- src/common/API.js
+++ src/common/API.js
@@ -2,20 +2,15 @@
 const config = require('./config');
 var cloudinary = require("cloudinary-core");
 
-var cl = new cloudinary.Cloudinary({
-    cloud_name: config.CLOUD_NAME,
-    api_key: config.API_KEY,
-    api_secret: config.API_SEC
-});
+var cl = new cloudinary.Cloudinary();
 
 //Generic API Request
 async function apiRequest(url, method, data, callback) {
 
-    let options = {
-        method: method
-    }
+    let options = {}
 
     if (method === 'POST') {
+        options['method'] = method
         options['body'] = data
     }
 
@@ -35,16 +30,15 @@ function uploadFile(file, callback) {
     const formData = new FormData();
     formData.append('file', file[0]);
     formData.append('upload_preset', 'ml_default');
-    formData.append('tags', 'test');
 
-    const url = `https://api.cloudinary.com/v1_1/${config.CLOUD_NAME}/upload`;
+    const url = `${config.API_URL}/upload`;
     apiRequest(url, 'POST', formData, callback)
 }
 
 //Fetch all Images
 function getImages(callback) {
-    const url = `https://res.cloudinary.com/${config.CLOUD_NAME}/image/list/test.json`;
-    apiRequest(url, 'GET', {}, callback)
+    const url = `http://res.cloudinary.com/${config.CLOUD_NAME}/image/list/test.json`;
+    apiRequest(url, '', {}, callback)
 }
 
 
--- src/common/config.js
+++ src/common/config.js
@@ -1,9 +1,10 @@
 
 const config = {
-    API_URL: '	https://api.cloudinary.com/v1_1/dzo2od8kf',
+    API_URL: '	https://api.cloudinary.com/v1_1/yobucket',
+    API_BASE_URL: '	https://api.cloudinary.com/',
     API_KEY: '911828417765431',
     API_SEC: 'wAlstcmrCu1awCdBexA8b5ZaNxQ',
-    CLOUD_NAME: 'dzo2od8kf'
+    CLOUD_NAME: 'yobucket'
 }
 
 
